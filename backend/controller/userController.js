f30cbefe6078f102bfa19fe2e24b65d34beb67fd
SyedAliImran <syed.ali.imran456@gmail.com>
2022-08-02 05:25:24 +0500

    Update userController.js
    procte route to prevent changes from differents user to different user goal

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,12 +1,12 @@
 const asyncHandler = require("express-async-handler");
 
 const userGoalScehma = require("../models/userGoalModel");
-
+const userModel = require("../models/userModel");
 //@dec     getUserGoals
 //@route   GET/api/users
 //@access  Private
 const getUserGoal = asyncHandler(async (req, res) => {
-  const userGoals = await userGoalScehma.find({user: req.user.id});
+  const userGoals = await userGoalScehma.find({ user: req.user.id });
   res.status(200).json(userGoals);
 });
 
@@ -20,7 +20,7 @@ const createUserGoal = asyncHandler(async (req, res) => {
   }
   const createGoal = await userGoalScehma.create({
     goal: req.body.goal,
-    user: req.user.id
+    user: req.user.id,
   });
 
   res.status(200).json(createGoal);
@@ -35,11 +35,27 @@ const updateUserGoal = asyncHandler(async (req, res) => {
     res.status(400);
     throw new Error("Goal not found");
   }
+  //---------------- find loged in user in usermodel
+  const user = await userModel.findById(req.user.id);
+  console.log(user)
+  console.log('I AM ID user._id '+user._id)
+  console.log('I AM ID userGoalScehma.user '+userGoalScehma.goal)
+
+  if (!user) {
+    res.status(401);
+    throw new Error("User Not Found");
+  }
+  //-- make sure corect user loggin
+  if (userGoalScehma.user !== user._id) {
+    res.status(401);
+    throw new Error("Not Authorized ");
+  }
+
   const updateGoal = await userGoalScehma.findByIdAndUpdate(
     req.params.id,
     {
       goal: req.body.goal,
-    }, 
+    },
     { new: true }
   );
 
@@ -55,6 +71,7 @@ const deleteUserGoal = asyncHandler(async (req, res) => {
     res.status(400);
     res.send({ error: "goal not found" });
   }
+ 
   await deleteGoalId.remove();
   res.status(200).json({ id: `delete goal id ${req.params.id}` });
 });
